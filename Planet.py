c23f9f6c8200094200aa062c99b75d9272a9293e
alexandre <alexandre.ward@ecole-hexagone.com>
2021-05-16 19:10:24 +0200

    add planets

---
+++ Planet.py
@@ -0,0 +1,17 @@
+import pygame
+import random
+
+class Planet:
+    def __init__(self, Surface, position) -> None:
+        RedGreenBlue = [(255,0,0),(0,255,0),(0,0,255)]
+        self.PlanetSize = random.randint(10, 50)
+        self.PlanetMass = self.PlanetSize * 5
+
+        #Planet drawing
+        pygame.draw.circle(Surface, RedGreenBlue[random.randint(0,2)], position, self.PlanetSize, width=self.PlanetSize)
+
+        #gravity drawing
+        pygame.draw.circle(Surface, (255,255,255), position, self.PlanetSize+(self.PlanetSize*2), width=1)
+    
+    def getMass(self):
+        return self.PlanetMass
---
+++ main.py
@@ -0,0 +1,16 @@
+from Planet import Planet
+import pygame
+
+running = True
+
+src = pygame.display.set_mode((500, 500))
+while running:
+    pygame.init()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            circ = Planet(src, pygame.mouse.get_pos())
+            print(circ.PlanetMass)
+    pygame.display.flip()
+    
