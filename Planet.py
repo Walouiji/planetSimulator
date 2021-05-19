ed5254cb82ed5cdeb3296dd7db0da961aab399c4
alexandre <alexandre.ward@ecole-hexagone.com>
2021-05-20 01:43:22 +0200

    add set_velocity method

--- Planet.py
+++ Planet.py
@@ -8,10 +8,14 @@ class Planet:
         self.PlanetMass = self.PlanetSize * 5
 
         #Planet drawing
-        planet = pygame.draw.circle(Surface, RedGreenBlue[random.randint(0,2)], position, self.PlanetSize, width=self.PlanetSize)
+        self.planet = pygame.draw.circle(Surface, RedGreenBlue[random.randint(0,2)], position, self.PlanetSize, width=self.PlanetSize)
 
         #gravity drawing
         gravity = pygame.draw.circle(Surface, (255,255,255), position, self.PlanetSize+(self.PlanetSize*2), width=1)
     
     def getMass(self):
         return self.PlanetMass
+    
+    def set_velocity(planet):
+        planet.x += 10
+        pass
